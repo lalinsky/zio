61345a823fcf87be2cbcb9fb53b4b4982b104b60
Lukas Lalinsky <lukas@lalinsky.com>
2025-11-06 14:14:20 +0100

    Stub

--- src/event/backend.zig
+++ src/event/backend.zig
@@ -2,6 +2,7 @@ const builtin = @import("builtin");
 
 pub const Backend = switch (builtin.os.tag) {
     .linux => @import("backends/epoll.zig"),
+    .macos, .freebsd, .netbsd => @import("backends/kqueue.zig"),
     // TODO: implement io_uring, kqueue, iocp
     // TODO: add build option for explicit selection
     else => @import("backends/poll.zig"),
