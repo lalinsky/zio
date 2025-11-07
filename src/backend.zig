433b2a2bea0fa252f524be2e29c660dbffb8f5bb
Lukas Lalinsky <lukas@lalinsky.com>
2025-11-07 08:38:26 +0100

    Fix backend selection

--- src/backend.zig
+++ src/backend.zig
@@ -1,8 +1,28 @@
 const builtin = @import("builtin");
+const std = @import("std");
+const zevent_options = @import("zevent_options");
 
-pub const Backend = switch (builtin.os.tag) {
-    .linux => @import("backends/epoll.zig"),
-    // TODO: implement io_uring, kqueue, iocp
-    // TODO: add build option for explicit selection
-    else => @import("backends/poll.zig"),
+pub const Backend = blk: {
+    if (zevent_options.backend) |backend_name| {
+        if (std.mem.eql(u8, backend_name, "io_uring")) {
+            break :blk @import("backends/io_uring.zig");
+        } else if (std.mem.eql(u8, backend_name, "epoll")) {
+            break :blk @import("backends/epoll.zig");
+        } else if (std.mem.eql(u8, backend_name, "kqueue")) {
+            break :blk @import("backends/kqueue.zig");
+        } else if (std.mem.eql(u8, backend_name, "iocp")) {
+            break :blk @import("backends/iocp.zig");
+        } else if (std.mem.eql(u8, backend_name, "poll")) {
+            break :blk @import("backends/poll.zig");
+        } else {
+            @compileError("Unknown backend: " ++ backend_name);
+        }
+    }
+
+    // Default backend based on OS
+    break :blk switch (builtin.os.tag) {
+        .linux => @import("backends/epoll.zig"),
+        // TODO: implement io_uring, kqueue, iocp
+        else => @import("backends/poll.zig"),
+    };
 };
