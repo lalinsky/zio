72b3c3fc736f06d559bc1a44972958736b5c5810
Lukas Lalinsky <lukas@lalinsky.com>
2025-12-06 09:42:54 +0100

    Fix build on Zig 0.16

--- build.zig
+++ build.zig
@@ -65,7 +65,7 @@ pub fn build(b: *std.Build) void {
         .{ .name = "tcp-echo-server-stdio", .file = "examples/tcp_echo_server_stdio.zig" },
         .{ .name = "tcp-client", .file = "examples/tcp_client.zig" },
         .{ .name = "http-server", .file = "examples/http_server.zig" },
-        .{ .name = "tls-demo", .file = "examples/tls_demo.zig" },
+        //.{ .name = "tls-demo", .file = "examples/tls_demo.zig" },
         .{ .name = "mutex-demo", .file = "examples/mutex_demo.zig" },
         .{ .name = "producer-consumer", .file = "examples/producer_consumer.zig" },
         .{ .name = "signal-demo", .file = "examples/signal_demo.zig" },
