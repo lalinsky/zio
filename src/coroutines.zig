5b0c69b35feb5c641fd11ba401cbc662b4a7ec4b
Lukas Lalinsky <lukas@lalinsky.com>
2026-01-04 19:40:11 +0100

    Only dump stack trace on test failure

--- src/coroutines.zig
+++ src/coroutines.zig
@@ -942,12 +942,13 @@ test "Coroutine: stack trace" {
         coro.step();
     }
 
-    if (builtin.zig_version.major == 0 and builtin.zig_version.minor < 16) {
-        std.debug.dumpStackTrace(test_data.trace);
-    } else {
-        std.debug.dumpStackTrace(&test_data.trace);
-    }
-
-    try std.testing.expect(test_data.trace.index > 1 and test_data.trace.index < 7);
+    std.testing.expect(test_data.trace.index > 1 and test_data.trace.index < 7) catch |err| {
+        if (builtin.zig_version.major == 0 and builtin.zig_version.minor < 16) {
+            std.debug.dumpStackTrace(test_data.trace);
+        } else {
+            std.debug.dumpStackTrace(&test_data.trace);
+        }
+        return err;
+    };
 }
 
