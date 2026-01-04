40ce6fef061fa18ae503817039aa1540024b6775
Lukas Lalinsky <lukas@lalinsky.com>
2026-01-04 07:00:50 +0100

    Remove utils.zig and concurrent_queue_bench

--- src/zio.zig
+++ src/zio.zig
@@ -42,8 +42,6 @@ pub const wait = @import("select.zig").wait;
 pub const SelectResult = @import("select.zig").SelectResult;
 pub const WaitResult = @import("select.zig").WaitResult;
 
-pub const util = @import("utils.zig");
-
 pub const Io = @import("stdio.zig").Io;
 
 test {
