460ecffec398ec2feea66b4b21afb9e64e340a52
Lukas Lalinsky <lukas@lalinsky.com>
2026-01-10 13:47:40 +0100

    Use std.Io directly instead of stdio.Io alias

--- src/runtime.zig
+++ src/runtime.zig
@@ -967,11 +967,11 @@ pub const Runtime = struct {
         return mod.now();
     }
 
-    pub fn io(self: *Runtime) stdio.Io {
+    pub fn io(self: *Runtime) std.Io {
         return stdio.fromRuntime(self);
     }
 
-    pub fn fromIo(io_: stdio.Io) !*Runtime {
+    pub fn fromIo(io_: std.Io) !*Runtime {
         if (io_.vtable != &stdio.vtable) return error.InvalidIo;
         return stdio.toRuntime(io_);
     }
