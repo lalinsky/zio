0ded0ccf156723f16d64b0ee0d2dcc39ce547b9e
Lukas Lalinsky <lukas@lalinsky.com>
2026-02-05 06:45:38 +0100

    Use generic pointer type for __ulock_wait and __ulock_wake addr parameter

--- src/os/darwin.zig
+++ src/os/darwin.zig
@@ -12,8 +12,8 @@ pub const UL_COMPARE_AND_WAIT: u32 = 1;
 pub const ULF_WAKE_THREAD: u32 = 0x100;
 pub const ULF_WAKE_ALL: u32 = 0x200;
 
-pub extern "c" fn __ulock_wait(operation: u32, addr: *u32, value: u64, timeout_us: u32) c_int;
-pub extern "c" fn __ulock_wake(operation: u32, addr: *u32, wake_value: u64) c_int;
+pub extern "c" fn __ulock_wait(operation: u32, addr: ?*anyopaque, value: u64, timeout_us: u32) c_int;
+pub extern "c" fn __ulock_wake(operation: u32, addr: ?*anyopaque, wake_value: u64) c_int;
 
 // os_unfair_lock operations
 // Efficient low-level lock (macOS 10.12+, iOS 10.0+)
